0fd780d6d0a0940020447428aa27f061fc2c2cbd
James Dobbe <dobbe2@gmail.com>
2020-09-24 23:52:33 -0700

    started writing the logic to make the returned values random

---
+++ script.js
@@ -0,0 +1,22 @@
+//setting variables for the HTML elements
+let generateButton = document.body.querySelector("#generate-button");
+let answerDiv = document.body.querySelector("#random-answer");
+
+//creating arrays of random values to use for page
+let subjectOne= ["dinosaur", "cow", "race car", "sun", "mouse"];
+let actionOne= ["eat", "fight", "fly", "tackle", "smell", "jump"];
+let subjectTwo= ["car", "nerf gun", "swimming pool", "jelly"];
+
+let generatedAnswer = [];
+//creating a listener for our random generator
+generateButton.addEventListener("click", function(){
+    let randomAnswer = subjectOne[Math.floor(Math.random() * subjectOne.length)];
+    generatedAnswer.push(randomAnswer);
+    console.log(randomAnswer);
+    console.log(generatedAnswer);
+    randomAnswer = actionOne[Math.floor(Math.random() * actionOne.length)];
+    generatedAnswer.push(randomAnswer)
+    randomAnswer = subjectTwo[Math.floor(Math.random() * subjectTwo.length)];
+    generatedAnswer.push(randomAnswer);
+
+})
