e12543c074c9cbc84db708f18218a56e49f54130
James Dobbe <dobbe2@gmail.com>
2020-09-25 14:24:25 -0700

    updated logic for generating multiple selections from arrays into a one line equation

--- script.js
+++ script.js
@@ -10,13 +10,14 @@ let subjectTwo= ["car", "nerf gun", "swimming pool", "jelly"];
 let generatedAnswer = [];
 //creating a listener for our random generator
 generateButton.addEventListener("click", function(){
-    let randomAnswer = subjectOne[Math.floor(Math.random() * subjectOne.length)];
+    generatedAnswer = [];
+    let randomAnswer = subjectOne[Math.floor(Math.random() * subjectOne.length)] + ` ` + actionOne[Math.floor(Math.random() * actionOne.length)] + ` ` + subjectTwo[Math.floor(Math.random() * subjectTwo.length)];
     generatedAnswer.push(randomAnswer);
     console.log(randomAnswer);
     console.log(generatedAnswer);
-    randomAnswer = actionOne[Math.floor(Math.random() * actionOne.length)];
-    generatedAnswer.push(randomAnswer)
-    randomAnswer = subjectTwo[Math.floor(Math.random() * subjectTwo.length)];
-    generatedAnswer.push(randomAnswer);
+    // randomAnswer = actionOne[Math.floor(Math.random() * actionOne.length)];
+    // generatedAnswer.push(randomAnswer)
+    // randomAnswer = subjectTwo[Math.floor(Math.random() * subjectTwo.length)];
+    // generatedAnswer.push(randomAnswer);
 
 })
