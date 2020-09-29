cf24fb2952dc2f27eb16710f9c0f943c2570e6cf
James Dobbe <dobbe2@gmail.com>
2020-09-29 09:25:47 -0700

    added some styling, cleaned up and commented JS code

--- script.js
+++ script.js
@@ -10,18 +10,19 @@ let subjectTwo= ["a clown car", "a nerf gun", "a swimming pool", "a jellyfish", 
 let generatedAnswer = [];
 //creating a listener for our random generator
 generateButton.addEventListener("click", function(){
+    //empty the array for our next generated answer
     generatedAnswer = [];
+    //gran a random value from all 3 arrays and combine them into 1 long string
     let randomAnswer = subjectOne[Math.floor(Math.random() * subjectOne.length)] + ` ` + actionOne[Math.floor(Math.random() * actionOne.length)] + ` ` + subjectTwo[Math.floor(Math.random() * subjectTwo.length)];
+    //push the random anwswer to the generated answer array;
     generatedAnswer.push(randomAnswer);
+    //create an h3 element
     let drawingIdea = document.createElement("h3");
-    drawingIdea.innerHTML = `draw a ` + generatedAnswer;
-    answerDiv.append(drawingIdea)
-    // alert(`draw a ` + generatedAnswer + ` if you can` )
+    //add the text value of generatedAnswer to the H3 element
+    drawingIdea.innerHTML = `Draw a ` + generatedAnswer;
+    //add the h3 to the page
+    answerDiv.prepend(drawingIdea)
     console.log(randomAnswer);
     console.log(generatedAnswer);
-    // randomAnswer = actionOne[Math.floor(Math.random() * actionOne.length)];
-    // generatedAnswer.push(randomAnswer)
-    // randomAnswer = subjectTwo[Math.floor(Math.random() * subjectTwo.length)];
-    // generatedAnswer.push(randomAnswer);
 
 })
